dad04a8f56332ade205ca086db46329e4eaf163a
vanshsaini15 <92376023+vanshsaini15@users.noreply.github.com>
2022-04-12 15:48:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo } from "react";
 
-function App() {
+let runningTimeout;
+
+function DataList() {
+
+  const [userList, setUserList] = useState([]);
+  const [searchName, setSearchName] = useState("");
+  const [searchUsername, setSearchUsername] = useState("");
+  const [searchEmail, setSearchEmail] = useState("");
+  const [searchPhone, setSearchPhone] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+
+  const fetchData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUserList(data)
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+
+
+  let filteredData = useMemo(() => {
+    return userList.filter(data => {
+      if (searchName === '' && searchEmail === '' && searchUsername === '' && searchPhone === '') {
+        return data
+      }
+      else if (
+        data.name.toLowerCase().includes(searchName.toLowerCase())
+        && data.email.toLowerCase().includes(searchEmail.toLowerCase())
+        && data.username.toLowerCase().includes(searchUsername.toLowerCase())
+        && data.phone.toLowerCase().includes(searchPhone.toLowerCase())) {
+        return data;
+      }
+    })
+  }, [searchName, searchUsername, searchEmail, searchPhone, userList])
+
+
+  const debouncedOnChange = (e) => {
+    const value = e.target.value
+    setInputValue(value)
+    if (runningTimeout) clearTimeout(runningTimeout);
+    runningTimeout = setTimeout(() => {
+      setSearchName(value)
+
+    }, 1000)
+
+  }
+
+  useEffect(() => {
+    console.log('api call')
+  }, [searchName])
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="table">
+
+      <table>
+        <thead>
+
+          <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+
+          <tr>
+            <td>
+              <input type="text"
+                value={inputValue}
+                onChange={debouncedOnChange}
+                placeholder="Search"
+              />
+
+            </td>
+            <td>
+              <input type="text"
+                // value={userList}
+                onChange={(e) => setSearchUsername(e.target.value)}
+                placeholder="Search"
+              />
+
+            </td>
+            <td>
+              <input type="text"
+                // value={userList}
+                onChange={(e) => setSearchEmail(e.target.value)}
+                placeholder="Search"
+              />
+
+            </td>
+            <td>
+              <input type="text"
+                // value={userList}
+                onChange={(e) => setSearchPhone(e.target.value)}
+                placeholder="Search"
+              />
+
+            </td>
+
+          </tr>
+        </thead>
+
+        <tbody id="body">
+
+          {
+            filteredData.map(user => {
+              return (<tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+              </tr>)
+            })}
+        </tbody>
+
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default DataList;
+
