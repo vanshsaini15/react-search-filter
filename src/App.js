19d6ab962eb291ed6df7af1f1383ef0b52316fe0
vanshsaini15 <92376023+vanshsaini15@users.noreply.github.com>
2022-04-15 09:05:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,52 @@
 import React, { useState, useEffect, useMemo, useReducer } from "react";
 
-// const initialState ={
-//   userList: [],
-//   searchName: "",
-//   searchUsername: "",
-//   searchEmail: "",
-//   searchPhone: "",
-//   inputValue: "",
-//   name: "",
-//   email: "",
-//   phone: "",
-//   username: ""
-// }
+const initialState = {
+  searchName: "",
+  searchUsername: "",
+  searchEmail: "",
+  searchPhone: "",
+  inputValue: "",
+  // name: "",
+  // email: "",
+  // phone: "",
+  // username: ""
+};
 
-// const reducerFunc = (state, action) =>{
-//   switch (action.type){
-//     case ("UpdateItem"):
-//       return {...state, item: action.payload}
+const reducerFunc = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    // case ("UpdateItem"):
+    //   return {...state, item: action.payload}
 
-//     default:
-//       return{ ...state, ...action}
-//   }
-// }
+    default:
+      return { ...state, ...action };
+  }
+};
 
 let runningTimeout;
 
 function DataList() {
   const [userList, setUserList] = useState([]);
-  const [searchName, setSearchName] = useState("");
-  const [searchUsername, setSearchUsername] = useState("");
-  const [searchEmail, setSearchEmail] = useState("");
-  const [searchPhone, setSearchPhone] = useState("");
-  const [inputValue, setInputValue] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [username, setUsername] = useState("");
   const [userId, setUserId] = useState("");
+  // const [searchName, setSearchName] = useState("");
+  // const [searchUsername, setSearchUsername] = useState("");
+  // const [searchEmail, setSearchEmail] = useState("");
+  // const [searchPhone, setSearchPhone] = useState("");
+  // const [inputValue, setInputValue] = useState("");
 
-  // const [state, dispatch] = useReducer(reducerFunc, initialState)
+  const [state, dispatch] = useReducer(reducerFunc, initialState);
 
-  // const handleEvent = (e) => {
-  //   const value = e.target.value
-  //   const id = e.target.id
-  //   dispatchEvent({[id]: value})
-  // }
+  const handleEvent = (e) => {
+    const value = e.target.value;
+    const id = e.target.id;
+    dispatch({ [id]: value });
+
+    console.log("working");
+  };
 
   const fetchData = () => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -63,35 +65,44 @@ function DataList() {
   let filteredData = useMemo(() => {
     return userList.filter((data) => {
       if (
-        searchName === "" &&
-        searchEmail === "" &&
-        searchUsername === "" &&
-        searchPhone === ""
+        state.searchName === "" &&
+        state.searchEmail === "" &&
+        state.searchUsername === "" &&
+        state.searchPhone === ""
       ) {
         return data;
       } else if (
-        data.name.toLowerCase().includes(searchName.toLowerCase()) &&
-        data.email.toLowerCase().includes(searchEmail.toLowerCase()) &&
-        data.username.toLowerCase().includes(searchUsername.toLowerCase()) &&
-        data.phone.toLowerCase().includes(searchPhone.toLowerCase())
+        data.name.toLowerCase().includes(state.searchName.toLowerCase()) &&
+        data.email.toLowerCase().includes(state.searchEmail.toLowerCase()) &&
+        data.username
+          .toLowerCase()
+          .includes(state.searchUsername.toLowerCase()) &&
+        data.phone.toLowerCase().includes(state.searchPhone.toLowerCase())
       ) {
         return data;
       }
     });
-  }, [searchName, searchUsername, searchEmail, searchPhone, userList]);
+  }, [
+    state.searchName,
+    state.searchUsername,
+    state.searchEmail,
+    state.searchPhone,
+    userList,
+  ]);
 
   const debouncedOnChange = (e) => {
     const value = e.target.value;
-    setInputValue(value);
+    const id = e.target.id;
+
     if (runningTimeout) clearTimeout(runningTimeout);
     runningTimeout = setTimeout(() => {
-      setSearchName(value);
+      dispatch({ [id]: value });
     }, 600);
   };
 
   useEffect(() => {
     console.log("api call");
-  }, [searchName]);
+  }, [state.searchName]);
 
   function selectUser(id) {
     let data = userList[id - 1];
@@ -133,33 +144,36 @@ function DataList() {
           <tr>
             <td>
               <input
+                id="searchName"
                 type="text"
-                value={inputValue}
                 onChange={debouncedOnChange}
                 placeholder="Search"
               />
             </td>
             <td>
               <input
+                id="searchUsername"
                 type="text"
-                // value={userList}
-                onChange={(e) => setSearchUsername(e.target.value)}
+                value={state.searchUsername}
+                onChange={handleEvent}
                 placeholder="Search"
               />
             </td>
             <td>
               <input
+                id="searchEmail"
                 type="text"
-                // value={userList}
-                onChange={(e) => setSearchEmail(e.target.value)}
+                value={state.searchEmail}
+                onChange={handleEvent}
                 placeholder="Search"
               />
             </td>
             <td>
               <input
+                id="searchPhone"
                 type="text"
-                // value={userList}
-                onChange={(e) => setSearchPhone(e.target.value)}
+                value={state.searchPhone}
+                onChange={handleEvent}
                 placeholder="Search"
               />
             </td>
